eab55c863dc36546b769b4c646db9601ae40732d
riotconta014-coder <riotconta014@gmail.com>
2026-02-03 20:25:19 -0300

    Update server.js

--- gestao-gastos-web/backend/server.js
+++ gestao-gastos-web/backend/server.js
@@ -17,7 +17,7 @@ app.get('/gastos', (req, res) => {
   ]);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Servidor rodando');
 });
